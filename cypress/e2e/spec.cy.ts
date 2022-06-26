1dfea5f9ce5ef96cc311a90ad617c94e9b0482b2
MichelleSambiase <michellesambiase@gmail.com>
2022-06-26 16:21:55 -0300

    start testing with cypress

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,27 @@
+describe('My first test', () => {
+  it('Gets, types and asserts', () => {
+    cy.visit('https://example.cypress.io');
+
+    cy.contains('type').click();
+
+    // Should be on a new URL which
+    // includes '/commands/actions
+    cy.url().should('include', '/commands/actions');
+
+    // Get an input, type into it and verify
+    // that the value has been updated
+
+    /*
+    And there you have it: a short test in Cypress that visits a page, 
+    finds and clicks a link,
+    verifies the URL and then verifies the behavior of an element on the new page. 
+    If we read it out loud, it might sound like:
+    */
+
+    cy.get('.action-email')
+      .type('fake@email.com')
+      .should('have.value', 'fake@email.com');
+  });
+});
+
+export {};
